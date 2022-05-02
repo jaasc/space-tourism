4baad740d61305112b8e98bf20c69969b14a4367
Muning <muning@Munings-MacBook-Pro.local>
2022-05-02 13:41:21 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,5 @@ import './styles/destination.css';
 import './styles/crew.css';
 import './styles/technology.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+ReactDOM.render(<App />, document.getElementById("root"))
 
