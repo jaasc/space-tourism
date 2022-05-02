1414e0608be70ef8db397f74d39f8585746ad8b8
Muning <muning@Munings-MacBook-Pro.local>
2022-05-02 10:57:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Home from "./components/Home"
+import Crew from "./components/Crew"
+import Destination from "./components/Destination"
+import Technology from "./components/Technology"
+import logo from "./assets/shared/logo.svg"
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom"
 
 function App() {
+  const [open, setOpen] = useState(false)
+
+  function toggleNav(){
+    setOpen(!open)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="navBar">
+        <img className="logo" src={logo} alt="logo"/>
+        <hr className="nav-break"></hr>
+        <div className={`toggle-nav${open? "-active" : ""}`} onClick={toggleNav}></div>
+        <div className={`navBar-menu nav-open${open? "-active" : ""}`}>
+          <NavLink className={({isActive}) => (isActive? "navBar-link-active" : "navBar-link")} 
+            to="/"><strong>00</strong>HOME</NavLink>
+          <NavLink className={({isActive}) => (isActive? "navBar-link-active" : "navBar-link")} 
+            to="/destination"><strong>01</strong> DESTINATION</NavLink>
+          <NavLink className={({isActive}) => (isActive? "navBar-link-active" : "navBar-link")} 
+            to="/crew"><strong>02</strong> CREW</NavLink>
+          <NavLink className={({isActive}) => (isActive? "navBar-link-active" : "navBar-link")} 
+            to="/technology"><strong>03</strong> TECHNOLOGY</NavLink>
+        </div>
+      </div>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/destination" element={<Destination/>}/>
+          <Route path="/crew" element={<Crew/>}/>
+          <Route path="/technology" element={<Technology/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
